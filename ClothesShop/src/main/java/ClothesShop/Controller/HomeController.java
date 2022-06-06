d289ddbe5e4b99ee3046986027b5121e13a1011f
NguyenThanhLyNa-18130145 <18130145@st.hcmuaf.edu.vn>
2022-06-06 16:02:37 +0700

    Kết nối cơ sở dữ liệu MySQL sử dụng JDBC

--- ClothesShop/src/main/java/ClothesShop/Controller/HomeController.java
+++ ClothesShop/src/main/java/ClothesShop/Controller/HomeController.java
@@ -1,26 +1,33 @@
 package ClothesShop.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import ClothesShop.DAO.HomeDAO;
+
 @Controller
 public class HomeController {
-	//ModelAndView 2 phan, view tra ve cac giao dien jsp
-		//model set duwx lieu truyen ve cho view
-		//co 2 cach:
-		//ModelAndView mv=new ModelAndView("user/index");
-		//mv.setViewName("user/index");
+	@Autowired
+	HomeDAO homeDao;
+
+	// ModelAndView 2 phan, view tra ve cac giao dien jsp
+	// model set duwx lieu truyen ve cho view
+	// co 2 cach:
+	// ModelAndView mv=new ModelAndView("user/index");
+	// mv.setViewName("user/index");
 	@RequestMapping(value = { "/", "/trang-chu" }, method = RequestMethod.GET)
 	public ModelAndView Index() {
-		ModelAndView mv=new ModelAndView("user/index");
+		ModelAndView mv = new ModelAndView("user/index");
+		mv.addObject("slides", homeDao.GetDataSlide());
 		return mv;
 	}
-	
-	@RequestMapping(value = "/products")
-	public ModelAndView Products() {
-		ModelAndView mv=new ModelAndView("user/products");
+
+	@RequestMapping(value = "/product")
+	public ModelAndView Product() {
+		ModelAndView mv = new ModelAndView("user/product");
 		return mv;
 	}
 }
